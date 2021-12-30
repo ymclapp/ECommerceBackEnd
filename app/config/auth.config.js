d43dcf03cdecfb9b5651df27ebff87c1f1257d39
ymcla <ymclapp@yahoo.com>
2021-12-30 14:57:40 -0600

    created auth.config.js

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {  //jsonwebtoken functions such as verify() or sign() use algorithm that needs a secret key (as String) to encode and decode token
+    secret:  "bezkoder-secret-key"
+};
